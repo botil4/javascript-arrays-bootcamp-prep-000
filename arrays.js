6aafe36da14eb0fb6efbe6a94a7406a1339f4209
Travis Clarkson <tk1667@gmail.com>
2018-02-04 18:54:22 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -14,3 +14,18 @@ var grilledCheeseIngredients = [
     'basil',
     'oregano'
     ]
+    
+    var chocolateBars = [
+      'snickers',
+      'hundred grand',
+      'kitkat',
+      'skittles'
+      ]
+      
+      function addElementToBeginningOfArray(array, element) {
+        return array(element, ...array)
+      }
+      
+      function destructivelyAddElementToBeginningOfArray(array, element) {
+        
+      }
